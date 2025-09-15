f68f3a2306922015e5bb25ceac62c872e77ede14
Niklas <niklas.hinze@cloudogu.com>
2025-09-15 13:53:25 +0200

    #16 Add condition for logs and events

--- api/v1/supportarchive_types.go
+++ api/v1/supportarchive_types.go
@@ -11,6 +11,8 @@ const (
 	ConditionVolumeInfoFetched     = "VolumeInfoFetched"
 	ConditionNodeInfoFetched       = "NodeInfoFetched"
 	ConditionSecretsFetched        = "SecretsFetched"
+	ConditionEventsFetched         = "EventsFetched"
+	ConditionLogsFetched           = "LogsFetched"
 )
 
 // SupportArchiveSpec defines the desired state of SupportArchive.
